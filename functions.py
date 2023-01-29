4ace820fd9fb7d13a4913ef09573881618e36398
seregakriwoy <93329359+seregakriwoy@users.noreply.github.com>
2023-01-29 21:44:34 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,12 +11,12 @@ import random
 # from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-def if_in_table(st, srtname):
+def if_in_table(st, srtname, table_name):
     connect = sqlite3.connect('database.db')
     cursor = connect.cursor()
     while True:
         x = random.randint(100000, 999999)
-        if cursor.execute(f'SELECT {st} FROM classes WHERE {st}=?', (srtname + str(x),)).fetchone() != None:
+        if cursor.execute(f'SELECT {st} FROM {table_name} WHERE {st}=?', (srtname + str(x),)).fetchone() != None:
             continue
         else:
             return str(x)
@@ -27,7 +27,7 @@ def id_check():
     cursor = connect.cursor()
     while True:
         x = random.randint(100000, 999999)
-        if cursor.execute('SELECT id FROM classes WHERE id=?', (x,)).fetchone() != None:
+        if cursor.execute('SELECT class_id FROM classes WHERE class_id=?', (x,)).fetchone() != None:
             continue
         else:
             return x
